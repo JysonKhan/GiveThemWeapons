8f879725307a1e8b6ca8405755fa8cdcc0250389
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-26 15:25:21 -0400

    Create utils.js
    defined basic expectation for each function

---
+++ scripts/utils.js
@@ -0,0 +1,27 @@
+/*
+async randomWeapon(void){
+  let WeapList = populatelist(void){return listofweaponsavailable;};
+  let randWeap = new Roll(1d(sizeof(weaplist),,);
+  randWeap.evaluate();
+  return WeapList(randWeap.total);
+}
+*/
+/*
+boolean async needsWeapon(TDoc){
+  do until(sizeof(TDoc.actor.items) != x, x = 0, x++){
+    if (Tdoc.actor.item(x).name === "as weapon"){return true;}
+  }
+  return false;
+}
+*/
+/*
+async replaceWeapon(TDoc, weapon){
+  do until(sizeof(TDoc.actor.items) != x, x = 0, x++){
+    if (Tdoc.actor.item(x).name === "as weapon"){
+      deleteItem(Tdoc.actor.item(x));
+      addItem(TDoc, weapon);
+      return
+    }
+  }
+}
+*/
