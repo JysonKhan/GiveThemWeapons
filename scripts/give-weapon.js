17eb3b5c7ac5c8ff98a06e9f574abfd9f200274b
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-27 08:41:58 -0400

    Update give-weapon.js
    including the ulits.js file and setting up needWeapon checks

--- scripts/give-weapon.js
+++ scripts/give-weapon.js
@@ -1,8 +1,11 @@
+include "./utils.js";
+
 Hook.on("createToken", (TDoc, spl) => {
   if (TDoc.actor.type === "NPC") {
     if (needsWeapon(TDoc)) {
-      let rWeap = randomWeapon();
-      replaceWeapon(TDoc.actor.items("as weapon"), rWeap);
+      console.log(True);
+      //let rWeap = randomWeapon();
+      //replaceWeapon(TDoc.actor.items("as weapon"), rWeap);
     }
   }
 }
