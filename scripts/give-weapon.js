42ed8e25395cc25b072fa3a2310e88fcae0d2c79
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-27 12:54:39 -0400

    Update give-weapon.js
    adding imports to begin testing initial functionality

--- scripts/give-weapon.js
+++ scripts/give-weapon.js
@@ -1,10 +1,12 @@
-include "./utils.js";
+import {needsWeapon} from "./utils.js";
+import {randomWeapon} from "./utils.js";
 
 Hook.on("createToken", (TDoc, spl) => {
   if (TDoc.actor.type === "NPC") {
     if (needsWeapon(TDoc)) {
       console.log("True");
-      //let rWeap = randomWeapon();
+      let rWeap = randomWeapon();
+      console.log (rWeap);
       //replaceWeapon(TDoc.actor.items("as weapon"), rWeap);
     }
   }
