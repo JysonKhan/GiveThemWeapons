326de6287353b4f69a45f38e64d08afc4fb7cf41
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-27 12:39:41 -0400

    Update utils.js
    initial iteration of function randomWeapon ready for testing

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,12 +1,13 @@
-/*
-async randomWeapon(void){
-  let WeapList = populatelist(void){return listofweaponsavailable;};
-  let randWeap = new Roll(1d(sizeof(weaplist),,);
+
+function randomWeapon(){
+  let WeapList = ["@Item[tj6TZ5aqxUgy6sjw]{Battleaxe}","@Item[aBKNBAhnwlRMWO3B]{Blackjack}","@Item[ftfcg8TU3FGpUnfo]{Blowgun}","@Item[oJSCvdpiqKPvwNxf]{Club}"];
+  let NumWeap = "1d" + WeapList.size;
+  let randWeap = new Roll(NumWeap,0,0);
   randWeap.evaluate();
   return WeapList(randWeap.total);
 }
-*/
-boolean async Function needsWeapon(TDoc){
+
+function needsWeapon(TDoc){
   let x = 0;
   while (TDoc.actor.data.items.size != x) {
     if (Tdoc.actor.data.items(x).value.data.name === "as weapon"){return true;}
@@ -15,13 +16,15 @@ boolean async Function needsWeapon(TDoc){
   return false;
 }
 /*
-async replaceWeapon(TDoc, weapon){
-  do until(sizeof(TDoc.actor.items) != x, x = 0, x++){
+function replaceWeapon(TDoc, weapon){
+  let x = 0;
+  for(TDoc.actor.items){
     if (Tdoc.actor.item(x).name === "as weapon"){
       deleteItem(Tdoc.actor.item(x));
       addItem(TDoc, weapon);
       return
     }
+    x = x + 1;
   }
 }
 */
