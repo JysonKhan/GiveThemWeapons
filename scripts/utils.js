94b8c0b3d8af1549ee390c52c42fa3d39b34611e
JysonKhan <91392522+JysonKhan@users.noreply.github.com>
2021-09-29 17:06:24 -0400

    Update utils.js
    update with working code, still needs additional weapon keys added

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,30 +1,31 @@
-
 export function randomWeapon(){
-  let WeapList = ["@Item[tj6TZ5aqxUgy6sjw]{Battleaxe}","@Item[aBKNBAhnwlRMWO3B]{Blackjack}","@Item[ftfcg8TU3FGpUnfo]{Blowgun}","@Item[oJSCvdpiqKPvwNxf]{Club}"];
-  let NumWeap = "1d" + WeapList.size;
-  let randWeap = new Roll(NumWeap,0,0);
+  let WeapList = ["tj6TZ5aqxUgy6sjw", "aBKNBAhnwlRMWO3B", "ftfcg8TU3FGpUnfo", "oJSCvdpiqKPvwNxf"];
+  let NumWeap = "1d4-1";
+  let randWeap = new Roll(NumWeap);
   randWeap.evaluate();
-  return WeapList(randWeap.total);
-}
+  return WeapList[randWeap.total];
+};
 
 export function needsWeapon(TDoc){
-  let x = 0;
-  while (TDoc.actor.data.items.size != x) {
-    if (Tdoc.actor.data.items(x).value.data.name === "as weapon"){return true;}
-    x = x + 1;
+  for (let x = 0; TDoc.actor.data.items.size != x; x++) {
+    if (TDoc.actor.data.items.contents[x].data.name === "as weapon"){return true;}
   }
   return false;
-}
-/*
-function replaceWeapon(TDoc, weapon){
-  let x = 0;
-  for(TDoc.actor.items){
-    if (Tdoc.actor.item(x).name === "as weapon"){
-      deleteItem(Tdoc.actor.item(x));
-      addItem(TDoc, weapon);
-      return
-    }
-    x = x + 1;
-  }
-}
-*/
+};
+
+export async function replaceWeapon(TDoc, weapon){
+	for (let x = 0; TDoc.actor.items.size != x; x++){
+		if (TDoc.actor.items.contents[x].data.name === "as weapon"){
+			let oWeap = TDoc.actor.items.contents[x].id;
+			let cWeap = game.items.get(weapon);
+			console.log("and we have a ...");
+			console.log(cWeap);
+			console.log(oWeap);
+			await TDoc.actor.deleteEmbeddedDocuments("Item", [oWeap]);
+			await TDoc.actor.createEmbeddedDocuments("Item", [cWeap.data]);
+			
+			return;
+		}
+		
+	}
+};
